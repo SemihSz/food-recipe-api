47f35109e56197dffa03159bb9cfeabe19dbcde8
Semih <ibrahim.semih@gmail.com>
2023-10-01 00:36:42 +0300

    register request updates

--- authentication/src/main/java/com/food/recipe/api/model/request/RegisterRequest.java
+++ authentication/src/main/java/com/food/recipe/api/model/request/RegisterRequest.java
@@ -2,6 +2,8 @@ package com.food.recipe.api.model.request;
 
 import com.food.recipe.api.enums.RoleTypes;
 import com.food.recipe.api.validation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,8 +19,32 @@ public class RegisterRequest {
     @ValidPassword
     private String password;
 
+    @ValidPassword
+    private String rePassword;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @Email
     private String email;
 
-    private RoleTypes role;
+    private boolean isPrivate;
 
+    private String bioHeader;
+
+    private String bioDesc;
+
+    private String url;
+
+    private String gender;
+
+    private String phone;
+
+    @NotBlank
+    private String imageId;
+
+    private RoleTypes role;
 }
--- common-api/src/main/java/com/food/recipe/api/validation/PasswordValidator.java
+++ common-api/src/main/java/com/food/recipe/api/validation/PasswordValidator.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
  */
 public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
 
-    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{16,}$";
 
     @Override
     public void initialize(ValidPassword constraintAnnotation) {
